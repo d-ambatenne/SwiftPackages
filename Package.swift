05d86850142498781f2b68bfb1d9654c6360836c
Denis Ambatenne <75973078+d-ambatenne@users.noreply.github.com>
2024-06-11 16:34:42 +0200

    Create Package.swift
    New file

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+   name: "Shared",
+   platforms: [
+     .iOS(.v14),
+   ],
+   products: [
+      .library(name: "Shared", targets: ["Shared"])
+   ],
+   targets: [
+      .binaryTarget(
+         name: "Shared",
+         url: "<link to the uploaded XCFramework ZIP file>",
+         checksum:"<checksum calculated for the ZIP file>")
+   ]
+)
